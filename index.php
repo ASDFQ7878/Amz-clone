91ed60a1fa5aa77ea466d21131e9e7889a57c972
ASDFQ7878 <unapurkaryash@gmail.com>
2020-10-23 14:04:06 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <!-- font aws-->
+<script src="https://kit.fontawesome.com/86862bf9f2.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+ <link href="https://fonts.googleapis.com/css?family=Noto+Serif&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+  
+</head>
+<body>
+
+<!--*************************************Navbar start********************-->
+<section>  
+<nav class="navbar navbar-inverse " style="background-color: rgba(0,0,0,0.9);">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="admin/images/logo.png"></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      <li class="dropdown" style="margin-top: 10px">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">All
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+
+    
+<li>
+  <form class="navbar-form navbar-left" action="#" style="padding-top: 10px;" >
+                <div class="input-group">
+                  <input type="text" class="form-control"  placeholder="Search">
+                  <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit" style="background-color: #cd9042;">
+                      <i class="glyphicon glyphicon-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+</li>
+</ul>
+<ul class="nav navbar-nav navbar-right">
+          <li>
+            <div class="dropdown" style="margin-top: 9px">
+              <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" style="background-color: transparent;"><img src="admin/images/flag.png" style="width: 25px; border-radius: 2px;">
+              <span class="caret" style="color: white;"></span>
+            </button>
+              <ul class="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+              </ul>
+            </div>
+          </li>
+
+
+
+        <li>
+             <div class="dropdown" style="margin-top: 9px">
+              <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" style="background-color: transparent;">
+                 <a href="#"> <span style="color: #fff; font-size:13px;">Hello,Yash<br></span>
+
+
+        <span style="font-size: 14px; font-style: bold; color: #fff; font-weight: bold;">Account & Lists</span></a>
+              <span class="caret" style="color: white;"></span>
+            </button>
+              <ul class="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+              </ul>
+            </div>
+         </li>
+
+
+
+        <li><a href="#"><span style="color: #fff">Retuns <br></span><span style="font-size: 14px; font-style: bold; color: #fff; font-weight: bold;">& Orders</span></a></li>
+
+
+         <li>
+             <div class="dropdown" style="margin-top: 9px">
+              <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" style="background-color: transparent;">
+                 <a href="#"> <span style="color: #fff; font-size:13px;">Try<br></span>
+
+
+        <span style="font-size: 14px; font-style: bold; color: #fff; font-weight: bold;">Prime</span></a>
+              <span class="caret" style="color: white;"></span></button>
+              <ul class="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+              </ul>
+            </div>
+         </li>
+    
+       <li><a href="#"><i style="color: #fff" class="fa fa-shopping-cart fa-2x"></i><span style="color: #fff">Cart 1</span></a></li>
+     </ul>
+   </div>
+ </div>
+<!--*******************************new div start***************-->
+
+     <div class="container-fluid" style="background-color:#232F3E; padding-bottom: 4px;">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><i class="fa fa-map-marker" aria-hidden="true" style="color: #fff"></i>   <span style="color: #fff; font-size: 12px;font-weight: bold;">  Deliver to Yash<br></span>
+          <center> <span style="font-weight: bold;color:#fff; font-size: 16px;"> Pune 411041</span></center></a>
+    </div>
+    <ul class="nav navbar-nav navbar-left">
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" >
+        </a>
+      </li>
+       <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" >
+        </a>
+      </li>
+       
+      <li style="font-weight: bold; font-size: 13px;"><a href="#"><span>Yash's Amazon.in</span></a></li>
+      <li style="font-weight: bold; font-size: 13px;"><a href="#"><span>Amazon Pay</span></a></li>
+      <li style="font-weight: bold; font-size: 13px;"><a href="#"><span>Mobiles</span></a></li>
+      <li style="font-weight: bold; font-size: 13px;"><a href="#"><span>Best seller</span></a></li>
+      <li id="list_1" style="font-weight: bold; font-size: 13px;"><a href="#"><span>Today's Deals</span></a></li>
+      <li id="list_1" style="font-weight: bold; font-size: 13px;"><a href="#"><span>Customer Service</span></a></li>
+      <li id="list_1" style="font-weight: bold; font-size: 13px;"><a href="#"><span>Pantry</span></a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+      <div class="box" id="box1" style="border-color: white">
+      <a href="#"><h4 style="color: #fff"> Freedom Sale | Shop Now <span><i class="fa fa-angle-right" aria-hidden="true"></i></span> </h4></a>
+    </div>
+    </li>
+    </ul>
+  </div>
+<!--*******************************new div end******************-->
+<!--*********************************carousel Start**************************-->
+<section>
+  <center>
+ <div class="main-carousel" data-flickity='{ "cellAlign": "left", "pageDots": false, "wrapAround": true, "autoPlay": true, "contain": true }'>
+  <div class="gallery-cell"><img src="admin/images/slider1.jpg" ></div>
+  <div class="gallery-cell"><img src="admin/images/slider1.jpg" ></div>
+  <div class="gallery-cell"><img src="admin/images/slider1.jpg" ></div>
+  <div class="gallery-cell"><img src="admin/images/slider1.jpg" ></div>
+  <div class="gallery-cell"><img src="admin/images/slider1.jpg" ></div>
+</div>
+</center>
+</section>
+<!--***********************Carousel end**************************-->
+</nav>
+</section>
+<section><p style="color: transparent; background-color: #f0f0f0;">Seprator</p></section>
+<!--*****************************Nusta Div start******************************-->
+<div class="row" style="margin-left: 10px;margin-right: 10px; background-color: #fff">
+  <!--1st Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***1St card end***-->
+  <!--2 Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***2 card end***-->
+  <!--3 Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+          
+  </div></center><!----***3 card end***-->
+  <!--4 Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***4 card end***-->
+  
+</div>
+
+<!--*****************************Nusta Div end******************************-->
+<section><p style="color: transparent; background-color: #f0f0f0;">Seprator</p></section>
+
+<!--*****************************nusta div 2 start**************************-->
+<div class="row" style="margin-left: 10px;margin-right: 10px; background-color: #fff">
+  <!--1st Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***1St card end***-->
+  <!--2 Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***2 card end***-->
+  <!--3 Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***3 card end***-->
+  <!--4 Card start--><center><div class="col-sm-3">
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  </div></center><!----***4 card end***-->
+  
+</div>
+
+
+<!--*****************************nusta div 2 end****************************-->
+
+
+<!--***************************************Today's deal slider start**********************-->
+<section><p style="color: transparent; background-color: #f0f0f0;">Seprator</p></section>
+<section style="background-color: #fff; margin-left:20px; margin-right: 20px">
+<h3 style="margin-left: 10px;">Today's Sale <span style="font-size: 17px;"><a href="#"> See More</a></span></h3>
+  <div  style="margin-left:20px; margin-right:20px"> 
+  <center>
+ <div class="main-carousel" data-flickity='{ "cellAlign": "left","pageDots": false, "contain": true }'>
+  <div class="Today-cell"><img src="admin/images/tay.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/3.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/4.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/bullet.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/cbr250r.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/1.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/2.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/1.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/3.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/tay.jpg" class="img-responsive"></div>
+
+</div>
+</center>
+</section>
+
+
+<!--***************************************Today's deal slider end**********************-->
+<section><p style="color: transparent; background-color: #f0f0f0;">Seprator</p></section>
+
+
+<!--********************************Recommended slider start*******************-->
+<p style="color: transparent; background-color: #f0f0f0;">Seprator</p></section>
+<section style="background-color: #fff; margin-left:20px; margin-right: 20px">
+<h3 style="margin-left: 10px;">Recomended</h3>
+  <div  style="margin-left:20px; margin-right:20px"> 
+  <center>
+ <div class="main-carousel" data-flickity='{ "cellAlign": "left","pageDots": false, "contain": true }'>
+  <div class="Today-cell"><img src="admin/images/tay.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/3.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/4.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/bullet.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/cbr250r.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/1.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/2.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/1.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/3.jpg" class="img-responsive"></div>
+  <div class="Today-cell"><img src="admin/images/tay.jpg" class="img-responsive"></div>
+
+</div>
+</center>
+</section>
+
+
+<!--********************************Recommended slider End*******************-->
+<p style="color: transparent; background-color: #f0f0f0;">Seprator</p></section>
+
+<section>
+  <div>
+    
+  </div>
+</section>
+  
+  <!-- JavaScript -->
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+403702773
+497skx
