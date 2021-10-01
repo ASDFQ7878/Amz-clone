c8f0c9215016aec6d368319e274b6fcdd6a5d43b
YN78 <72245124+YN78@users.noreply.github.com>
2021-10-01 13:24:46 +0530

    Add this function

--- functions/function.php
+++ functions/function.php
@@ -25,4 +25,48 @@ function addCart(){
 }
 //************************header Stop*************************
 
+
+function getPro(){
+  
+     global $db;
+     $get_product="select * from products order by 1 DESC LIMIT 0,6";
+     $run_products= mysqli_query($db,$get_product);
+     while ($row_product=mysqli_fetch_array($run_products)) {
+     	$pro_id=$row_product['product_id'];
+     	$pro_title=$row_product['product_title'];
+     	$pro_price=$row_product['product_price'];
+     	$pro_img1=$row_product['product_img1'];
+      $pro_img2=$row_product['product_img2'];
+      $pro_img3=$row_product['product_img3'];
+
+        echo "<div class='col-md-3 col-sm-6'>
+         <div class='product'>
+       
+          <a href='details.php?pro_id=$pro_id'>
+            <img src='admin/product_images/$pro_img1' class='img-responsive'>
+           </a>
+           <center>
+           <div class='row'>
+           <img src='admin/product_images/$pro_img1'width='80'>
+            <img src='admin/product_images/$pro_img2'width='80'>
+             <img src='admin/product_images/$pro_img3'width='80'>
+           </div></center>
+            <div class='text'>
+             <h3><a href='details.php?pro_id=$pro_id'>$pro_title</a></h3>
+            <p class='price'>INR $pro_price </p>
+            <p class='buttons'>
+   			<a href='details.php?pro_id=$pro_id' class='btn btn-default'>View Details</a>
+   			<a href='details.php?pro_id=$pro_id' class='btn btn-primary'><i class='fa fa-shopping-cart'></i>Add To Cart</a>
+
+            </p>
+            </div>
+         </div>
+    </div>";
+
+
+     }
+ }
+
+/*****************product catgories start**************/
+
 ?>
