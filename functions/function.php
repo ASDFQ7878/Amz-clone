c4f1f54d7b3e19aa9c6d5ad38f61f00d59dbc8be
YN78 <72245124+YN78@users.noreply.github.com>
2021-10-01 13:18:46 +0530

    Add this function

--- functions/function.php
+++ functions/function.php
@@ -25,4 +25,15 @@ function addCart(){
 }
 //************************header Stop*************************
 
+/***************for user ip addr start****************/
+    function getUserIP(){
+        switch (true) {
+            case(!empty($_SERVER['HTTP_X_REAL_IP'])) :return $_SERVER['HTTP_X_REAL_IP'];
+            case(!empty($_SERVER['HTTP_CLIENT_IP'])) :return $_SERVER['HTTP_CLIENT_IP'];
+            case(!empty($_SERVER['HTTP_X_FOREARDED_FOR'])): return $_SERVER['HTTP_X_FOREARDED_FOR'];
+            default:return $_SERVER['REMOTE_ADDR'];
+        }
+    }
+
+
 ?>
