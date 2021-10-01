3e815c95e7340795d71f6497ca931abd6ddd0597
YN78 <72245124+YN78@users.noreply.github.com>
2021-10-01 19:35:01 +0530

    Update function.php

--- functions/function.php
+++ functions/function.php
@@ -25,4 +25,13 @@ function addCart(){
 }
 //************************header Stop*************************
 
+//////////////////////item count start//////////////////
+function item(){
+    global $db;
+    $ip_add=getUserIP();
+    $get_items="select * from cart where ip_add='$ip_add'";
+    $run_items=mysqli_query($db,$get_items);
+    $count=mysqli_num_rows($run_items);
+    echo $count;
+}
 ?>
