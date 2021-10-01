346ed42053755e7a956d0e9b554e8dc126a79c6b
YN78 <72245124+YN78@users.noreply.github.com>
2021-10-01 13:01:36 +0530

    update this function

--- functions/function.php
+++ functions/function.php
@@ -3,8 +3,14 @@
 
 
 //************************header start*************************
-function header()
-
+function item(){
+    global $db;
+    $ip_add=getUserIP();
+    $get_items="select * from cart where ip_add='$ip_add'";
+    $run_items=mysqli_query($db,$get_items);
+    $count=mysqli_num_rows($run_items);
+    echo $count;
+}
 
 //************************header Stop*************************
 
